761bef09969d35978ac9b3a08f45a3e06e6aa5c4
ggpwx <shandy.sandoro.55.ss@gmail.com>
2024-09-21 11:09:17 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,6 @@
 Config = {}
 Config.Crosshair = {
     shape = 'star',  -- 'circle', 'square', 'cross', 'star'
-    size = '5px',    -- Ukuran crosshair
-    color = '#ff0000' -- Warna crosshair
+    size = '5px',    -- Size crosshair
+    color = '#ff0000' -- Color crosshair
 }
